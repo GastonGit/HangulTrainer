5a32f2c7d608a8265d19712989f78a5a85aa12ca
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-07-07 20:58:01 +0200

    add mock buttons for play and options

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,8 +5,18 @@ const Menu = () => {
   return (
     <div>
       <div className="Menu-Button">
+        <Link to="/">
+          <button type="button">Play</button>
+        </Link>
+      </div>
+      <div className="Menu-Button">
         <Link to="/practice">
           <button type="button">Practice</button>
+        </Link>
+      </div>
+      <div className="Menu-Button">
+        <Link to="/">
+          <button type="button">Options</button>
         </Link>
       </div>
     </div>
