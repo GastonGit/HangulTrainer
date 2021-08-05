cefaf865214351dfe44f1364ebe0f33d4dd2cecb
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-08-05 14:03:51 +0200

    add form for changing play mode in options

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -4,10 +4,42 @@ class Options extends React.Component<unknown> {
   constructor(props: unknown) {
     super(props);
     this.state = {};
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
+  // eslint-disable-next-line class-methods-use-this
+  handleChange() {}
+
   render() {
-    return <div />;
+    return (
+      <div className="OptionList">
+        <div className="OptionItem">
+          <form className="Option-PlayMode">
+            <label htmlFor="mode1" className="">
+              Mode 1
+              <input
+                type="radio"
+                id="mode1"
+                name="playmode"
+                value="value"
+                onChange={this.handleChange}
+              />
+            </label>
+            <label htmlFor="mode2" className="">
+              Mode 2
+              <input
+                type="radio"
+                id="mode2"
+                name="playmode"
+                value="value"
+                onChange={this.handleChange}
+              />
+            </label>
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 
