416b004fed7d97313fa2b67017bdfdd7a34d319e
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-08-16 20:10:15 +0200

    Create general Hangul Mode class to be used for inheritence

---
+++ src/lib/modes/HangulModes.ts
@@ -0,0 +1,54 @@
+import GameMode from './GameModeInterface';
+import characters from '../../../assets/hangul-characters.json';
+import Character from './HangulCharacterInterface';
+
+class HangulMode1 implements GameMode {
+  list: Array<Character>;
+
+  resetList: Array<Character>;
+
+  currentIndex: number;
+
+  currentCharacter: Character;
+
+  constructor() {
+    this.list = JSON.parse(JSON.stringify(characters));
+    this.resetList = JSON.parse(JSON.stringify(this.list));
+    this.currentIndex = 0;
+    this.currentCharacter = this.list[this.currentIndex];
+  }
+
+  restartGame() {
+    this.list = JSON.parse(JSON.stringify(this.resetList));
+    this.currentIndex = 0;
+    this.currentCharacter = this.list[this.currentIndex];
+  }
+
+  getCurrentQuestionCount() {
+    return this.currentIndex + 1;
+  }
+
+  getFullQuestionCount() {
+    return this.list.length;
+  }
+
+  getCurrentHangul() {
+    return this.currentCharacter.hangul;
+  }
+
+  matchComplete() {
+    return !(this.currentIndex < this.list.length);
+  }
+
+  checkAnswer(input: string) {
+    if (input === this.currentCharacter.romanization) {
+      this.currentIndex += 1;
+      this.currentCharacter = this.list[this.currentIndex];
+
+      return true;
+    }
+    return false;
+  }
+}
+
+export default HangulMode1;
