46d9e3924a4f6eee7c1a323c8fe5af2a632ad94f
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-08-17 19:38:53 +0200

    refactor HangulMode2 to extend HangulModes

--- src/lib/modes/HangulMode2.ts
+++ src/lib/modes/HangulMode2.ts
@@ -1,16 +1,7 @@
-import GameMode from './GameModeInterface';
 import characters from '../../../assets/hangul-characters.json';
-import Character from './HangulCharacterInterface';
+import HangulModes from './HangulModes';
 
-class HangulMode2 implements GameMode {
-  list: Array<Character>;
-
-  resetList: Array<Character>;
-
-  currentIndex: number;
-
-  currentCharacter: Character;
-
+class HangulMode2 extends HangulModes {
   constructor() {
     const tempList = JSON.parse(JSON.stringify(characters));
     const finalList = [];
@@ -24,43 +15,7 @@ class HangulMode2 implements GameMode {
     for (let i = 0; i < charCount; i += 1) {
       finalList.push(tempList.pop());
     }
-
-    this.resetList = JSON.parse(JSON.stringify(finalList));
-    this.list = finalList;
-    this.currentIndex = 0;
-    this.currentCharacter = this.list[this.currentIndex];
-  }
-
-  restartGame() {
-    this.list = JSON.parse(JSON.stringify(this.resetList));
-    this.currentIndex = 0;
-    this.currentCharacter = this.list[this.currentIndex];
-  }
-
-  getCurrentQuestionCount() {
-    return this.currentIndex + 1;
-  }
-
-  getFullQuestionCount() {
-    return this.list.length;
-  }
-
-  getCurrentHangul() {
-    return this.currentCharacter.hangul;
-  }
-
-  matchComplete() {
-    return !(this.currentIndex < this.list.length);
-  }
-
-  checkAnswer(input: string) {
-    if (input === this.currentCharacter.romanization) {
-      this.currentIndex += 1;
-      this.currentCharacter = this.list[this.currentIndex];
-
-      return true;
-    }
-    return false;
+    super(finalList);
   }
 }
 
