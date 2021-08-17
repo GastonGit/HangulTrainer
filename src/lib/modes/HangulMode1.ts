e218dc732ebf9ad0029bbf21d2166c1e54da5a18
GastonGit <73943682+GastonGit@users.noreply.github.com>
2021-08-17 19:27:10 +0200

    have HangulMode1 extend HangulModes + cleanup

--- src/lib/modes/HangulMode1.ts
+++ src/lib/modes/HangulMode1.ts
@@ -1,51 +1,5 @@
-import GameMode from './GameModeInterface';
-import characters from '../../../assets/hangul-characters.json';
-import Character from './HangulCharacterInterface';
+import HangulModes from './HangulModes';
 
-class HangulMode1 implements GameMode {
-  list: Array<Character>;
-
-  currentIndex: number;
-
-  currentCharacter: Character;
-
-  constructor() {
-    this.list = JSON.parse(JSON.stringify(characters));
-    this.currentIndex = 0;
-    this.currentCharacter = this.list[this.currentIndex];
-  }
-
-  restartGame() {
-    this.list = JSON.parse(JSON.stringify(characters));
-    this.currentIndex = 0;
-    this.currentCharacter = this.list[this.currentIndex];
-  }
-
-  getCurrentQuestionCount() {
-    return this.currentIndex + 1;
-  }
-
-  getFullQuestionCount() {
-    return this.list.length;
-  }
-
-  getCurrentHangul() {
-    return this.currentCharacter.hangul;
-  }
-
-  matchComplete() {
-    return !(this.currentIndex < this.list.length);
-  }
-
-  checkAnswer(input: string) {
-    if (input === this.currentCharacter.romanization) {
-      this.currentIndex += 1;
-      this.currentCharacter = this.list[this.currentIndex];
-
-      return true;
-    }
-    return false;
-  }
-}
+class HangulMode1 extends HangulModes {}
 
 export default HangulMode1;
